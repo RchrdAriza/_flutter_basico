ac8f27b2127ff5857533fda7c1f09cddba4b3779
RchrdAriza <rchrdariza@gmail.com>
2023-12-21 21:01:50 -0500

    private

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,10 +9,49 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Alert Page"),
       ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            // shape: const StadiumBorder(),
+          ),
+          onPressed: () => _mostrarAlert(context),
+          child: const Text("alerta"),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pop(context),
         child: const Icon(Icons.arrow_back),
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Titulo'),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Este es el contenido de la caja de la alerta"),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancelar'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Ok'),
+              ),
+            ],
+          );
+        });
   }
 }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,12 +3,15 @@ import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_pages.dart';
 import 'package:componentes/src/pages/card_pages.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    "/": (BuildContext context) => HomePage(),
-    "alert": (BuildContext context) => AlertPage(),
-    "avatar": (BuildContext context) => AvatarPage(),
-    "card": (BuildContext context) => CardPage(),
+    "/": (BuildContext context) => const HomePage(),
+    "alert": (BuildContext context) => const AlertPage(),
+    "avatar": (BuildContext context) => const AvatarPage(),
+    "card": (BuildContext context) => const CardPage(),
+    "AnimatedContainer": (BuildContext context) =>
+        const AnimatedContainerPage(),
   };
 }
